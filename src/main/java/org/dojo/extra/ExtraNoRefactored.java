a8823c586f10f651d14c82167795568ba528e072
Zanete Hirona <zmolina@policyexpert.co.uk>
2024-04-09 13:03:54 +0100

    add more tests

--- src/main/java/org/dojo/extra/ExtraNoRefactored.java
+++ src/main/java/org/dojo/extra/ExtraNoRefactored.java
@@ -8,7 +8,7 @@ public class ExtraNoRefactored {
 
     public int getBagFees(int numberOfBags, int[] bagWeight, int bagFees) {
         for (int i = 0; i < numberOfBags; i++) {
-            if (i < 1) {
+            if (i <= 1) {
                 if (bagWeight[i] < 50) {
                     if (i == 0) {
                         bagFees += 25;
@@ -18,20 +18,14 @@ public class ExtraNoRefactored {
                 } else if (bagWeight[i] < 70) {
                     if (i == 0) {
                         bagFees += 100;
-                    } else if (i == 1) {
-                        bagFees += 150;
                     } else {
-                        bagFees += 200;
+                        bagFees += 150;
                     }
                 } else {
                     bagFees += 200;
                 }
-            } else if (i >= 0 && bagWeight[i] < 70) {
-                if (i == 1) {
-                    bagFees += 100;
-                } else {
-                    bagFees += 150;
-                }
+            } else if (bagWeight[i] < 70) {
+                bagFees += 150;
             } else {
                 bagFees += 200;
             }
--- src/test/java/org/example/service/extra/ExtraTests.java
+++ src/test/java/org/example/service/extra/ExtraTests.java
@@ -10,7 +10,7 @@ public class ExtraTests {
 
     @Test
     @DisplayName("Extra 1: Bags Fees Test")
-    void BagFeesTest() {
+    void BagFeesTest1() {
         //Given
         ExtraNoRefactored extraNoRefactored = new ExtraNoRefactored();
         int[] bagWeight = new int[]{25, 55, 75};
@@ -19,6 +19,48 @@ public class ExtraTests {
         //When
         bagFees = extraNoRefactored.getBagFees(numberOfBags, bagWeight, bagFees);
         //Then
-        assertEquals(325, bagFees);
+        assertEquals(375, bagFees);
+    }
+
+    @Test
+    @DisplayName("Extra 1: Bags Fees Test")
+    void BagFeesTest2() {
+        //Given
+        ExtraNoRefactored extraNoRefactored = new ExtraNoRefactored();
+        int[] bagWeight = new int[]{70, 55, 75};
+        int numberOfBags = bagWeight.length;
+        int bagFees = 0;
+        //When
+        bagFees = extraNoRefactored.getBagFees(numberOfBags, bagWeight, bagFees);
+        //Then
+        assertEquals(550, bagFees);
+    }
+
+    @Test
+    @DisplayName("Extra 1: Just one bag Test")
+    void BagFeesTest3() {
+        //Given
+        ExtraNoRefactored extraNoRefactored = new ExtraNoRefactored();
+        int[] bagWeight = new int[]{49};
+        int numberOfBags = bagWeight.length;
+        int bagFees = 0;
+        //When
+        bagFees = extraNoRefactored.getBagFees(numberOfBags, bagWeight, bagFees);
+        //Then
+        assertEquals(25, bagFees);
+    }
+
+    @Test
+    @DisplayName("Extra 1: Bags Fees Test")
+    void BagFeesTest4() {
+        //Given
+        ExtraNoRefactored extraNoRefactored = new ExtraNoRefactored();
+        int[] bagWeight = new int[]{75, 10, 10};
+        int numberOfBags = bagWeight.length;
+        int bagFees = 0;
+        //When
+        bagFees = extraNoRefactored.getBagFees(numberOfBags, bagWeight, bagFees);
+        //Then
+        assertEquals(385, bagFees);
     }
 }
